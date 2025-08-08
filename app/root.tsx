4aa40d5bb6a604ce9edd7d4e561a26b2b635fe92
Vu Ngo <ngoanhvu110293@gmail.com>
2025-08-08 20:11:33 +0700

    Add @vercel/react-router and update routing configuration; integrate MUI components in the app

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { StyledEngineProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -42,7 +44,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <StyledEngineProvider enableCssLayer>
+      <CssBaseline />
+      <Outlet />
+    </StyledEngineProvider>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,5 +1,6 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import Button from "@mui/material/Button";
 
 export function Welcome() {
   return (
@@ -19,6 +20,7 @@ export function Welcome() {
             />
           </div>
         </header>
+        <Button variant="contained">Click me</Button>
         <div className="max-w-[300px] w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
